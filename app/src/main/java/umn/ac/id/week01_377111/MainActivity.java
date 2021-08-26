33e3a1ae90b9ffa078f44cf89ae0c51a437d8c87
zeonplamo <zeonplamo@gmail.com>
2021-08-26 16:50:40 +0700

    latest
    Test

---
+++ app/src/main/java/umn/ac/id/week01_377111/MainActivity.java
@@ -0,0 +1,13 @@
+package umn.ac.id.week01_377111;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
